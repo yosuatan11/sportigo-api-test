a217d76e0050bd3c24d85742aa299c548fd71f39
yosuatan11 <yosua.tan1198@gmail.com>
2020-06-23 13:09:48 +0700

    add login function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class UserController extends Controller
 {
-    public function signup(Request $request)
+    public function register(Request $request)
     {
         $this->validate($request, [
             'username' => 'required',
@@ -24,5 +27,62 @@ class UserController extends Controller
         return response()->json([
             'message' => 'Successfully register user'
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $input = $request->only('username', 'password');
+
+        try{
+            if (!$token = JWTAuth::attempt($input)) {
+                return response()->json([
+                    'message' => 'Invalid Email or Password',
+                ], 401);
+            }
+        } catch (JWTException $e){
+            return response()->json([
+                'error' => 'Could not create token!'
+            ], 500);
+        }
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        try {
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'message' => 'Sorry, the user cannot be logged out'
+            ], 500);
+        }
+    }
+
+
+    public function getAuthUser(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        $user = JWTAuth::authenticate($request->token);
+
+        return response()->json(['user' => $user]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/user', [
-    'uses' => 'UserController@signup'
-]);
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'UserController@logout');
+
+    Route::get('user', 'UserController@getAuthUser');
+
+    
+});
